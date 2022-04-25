6aae0fe5e77931d153cc447634b22529cce087af
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-04-25 22:38:39 +0500

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Contact} from './pages/Contact'
+import { NotFound } from './pages/NotFound';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
 function App() {
-  return (
-    <>
-      <h1>Food project start</h1>
-    </>
-  );
+  return <>
+      <Header />
+        <main className='container content'>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route path="/contacts" component={Contact}/>
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </main>
+      <Footer />
+    </>;
 }
 
 export default App;
